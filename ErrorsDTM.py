7ccc2be53d2b6daef061727f89bf1604b2a2c5ba
oskretc <otorresbaca@gmail.com>
2013-07-30 12:50:24 +0200

    adding LengthError

--- ErrorsDTM.py
+++ ErrorsDTM.py
@@ -8,8 +8,23 @@ class ErrorsDTM(Exception):
 class FrequencyError(ErrorsDTM):
     def __init__(self, value):
         self.parameter = value
+        print value
         if value<0:
-            print "Frequency must be greater than 0, recieved: " + str(value) 
+            print "Frequency must be greater than 0, received: " + str(value) 
         elif value>39:
             print "Frequency must be less than 39, received: " + str(value) 
-        
+class PacketTypeError(ErrorsDTM):
+    def __init__(self, value):
+        self.parameter = value
+        if value<0:
+            print "Packet Type must be greater than 0, received: " + str(value) 
+        elif value>3:
+            print "Packet Type must be less than 3, received: " + str(value)       
+            
+class LengthError(ErrorsDTM):
+    def __init__(self, value):
+        self.parameter = value
+        if value<0:
+            print "Length must be greater than 0, received: " + str(value) 
+        elif value>0x25:
+            print "Length must be less than 37, received: " + str(value)                     
